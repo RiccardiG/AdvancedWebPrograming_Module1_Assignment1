1a94af37b1b695e053d76149c5f4362737f619db
RiccardiG <75377320+RiccardiG@users.noreply.github.com>
2022-10-09 22:54:18 -0700

    Assignment 2 work
    Added boostrap, CSS and JS & created html file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Server!')
-})
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
